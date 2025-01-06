d60709681cde97373141c4c21bde5ebfeb899787
andikalp313 <andikaluhurpambudi@gmail.com>
2025-01-07 01:26:23 +0700

    fix:bug

--- src/app/transactions/[id]/page.tsx
+++ src/app/transactions/[id]/page.tsx
@@ -4,7 +4,7 @@ import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState, ChangeEvent, FormEvent } from "react";
 import useGetTransaction from "@/hooks/api/transaction/useGetTransaction";
 
-import useUploadPaymentProof from "../../../hooks/api/transaction/usePaymentProof";
+import useUploadPaymentProof from "../../../hooks/api/transaction/seUploadPaymentProof";
 import { useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 import { AnimatePresence, motion } from "framer-motion";
