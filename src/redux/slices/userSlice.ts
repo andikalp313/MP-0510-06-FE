469b0cd37222cae998f6a85dca21414539a659dd
inas9116 <inas9116@gmail.com>
2025-01-08 02:49:19 +0700

    style: dashboard

--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "../store";
 
-export interface UserState {
+interface UserState {
   id: number;
   name: string;
   email: string;
@@ -12,6 +13,15 @@ const initialState: UserState = {
   name: "",
   email: "",
   token: "",
+  organizerName: "",
+  address: "",
+  role: "",
+  profilePicture: "",
+  referralCode: "",
+  points: "",
+  discountValue: "",
+  couponsExpiryDate: new Date(),
+  pointsExpiryDate: new Date(),
 };
 
 export const userSlice = createSlice({
@@ -23,15 +33,41 @@ export const userSlice = createSlice({
       state.name = action.payload.name;
       state.email = action.payload.email;
       state.token = action.payload.token;
+      state.organizerName = action.payload.organizerName;
+      state.address = action.payload.address;
+      state.role = action.payload.role;
+      state.profilePicture = action.payload.profilePicture;
+      state.referralCode = action.payload.referralCode;
+      state.points = action.payload.points;
+      state.discountValue = action.payload.discountValue;
+      state.couponsExpiryDate = action.payload.couponsExpiryDate;
+      state.pointsExpiryDate = action.payload.pointsExpiryDate;
     },
     logoutAction: (state) => {
       state.id = 0;
       state.name = "";
       state.email = "";
       state.token = "";
+      state.organizerName = "";
+      state.address = "";
+      state.role = "";
+      state.profilePicture = "";
+      state.referralCode = "";
+      state.points = "";
+      state.discountValue = "";
+      state.couponsExpiryDate = new Date();
+      state.pointsExpiryDate = new Date();
+    },
+    updateUserAction: (state, action: PayloadAction<Partial<UserState>>) => {
+      const { id, name, email, address, profilePicture } = action.payload;
+      if (id !== undefined) state.id = id;
+      if (name !== undefined) state.name = name;
+      if (email !== undefined) state.email = email;
+      if (address !== undefined) state.address = address;
+      if (profilePicture !== undefined) state.profilePicture = profilePicture;
     },
   },
 });
 
-export const { loginAction, logoutAction } = userSlice.actions;
+export const { loginAction, logoutAction, updateUserAction } = userSlice.actions;
 export default userSlice.reducer;
